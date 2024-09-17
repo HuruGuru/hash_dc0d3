627f81a86701236ae620a6edca5743d3a0c04b87
HuruGuru <tarlinni@gmail.com>
2024-09-17 21:59:56 +0500

    feat: added TODOs

--- hash_dc0d3.c
+++ hash_dc0d3.c
@@ -7,10 +7,12 @@ int main() {
     char str[N];
     unsigned char output[N];
 
+    // TODO: add dialog
     printf("Enter base64 string: ");
     scanf("%s", str);
 
     int decode_len = base64_decode(str, output);
+    // TODO: add func for encode
     if (decode_len > 0) {
         output[decode_len] = '\0';
         printf("Decoded output: %s\n", output);
